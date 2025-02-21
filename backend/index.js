00d92387c4f0b399520ed79994f4c583c28ae909
CharlesOsang017 <charlesosango02@gmail.com>
2025-02-21 17:28:50 +0300

    Connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,22 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import cookieParser from 'cookie-parser'
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { connectToDb } from "./config/db.js";
 
+dotenv.config();
 
-dotenv.config()
+const app = express();
 
-const app = express()
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
 
-app.use(express.json())
-app.use(cookieParser())
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(8000, ()=>{
-  console.log("server is running on port 8000")
-})
+// Start the server
+app.listen(PORT, async () => {
+  console.log(`Server is running on port ${PORT}`);
+  await connectToDb()
+});
