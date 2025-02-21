5da428ad3939bcb967a9f0b1432c76cb98d51f48
CharlesOsang017 <charlesosango02@gmail.com>
2025-02-21 17:55:36 +0300

    Add register user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,38 @@
+import User from "../models/user.model.js";
+import bcrypt from 'bcrypt'
+
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Please fill all the fields" });
+    }
+    // checking if email exists
+    const existingEmail = await User.findOne({ email });
+    if (existingEmail) {
+      return res.status(400).json({ message: "Email is already taken" });
+    }
+
+    // checking password length
+    if (password.length < 6) {
+      return res
+        .status(400)
+        .json({ message: "password should be 6 or more characters" });
+    }
+    // hashing the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+      name,
+      email,
+      password:hashedPassword,
+      role,
+    });
+    await newUser.save();
+    return res.status(201).json({ message: "user created successfully" });
+  } catch (error) {
+    console.log("error in registerUser controller", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
